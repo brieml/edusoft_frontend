369084c4b0e4e6d14c073576cab4466cec2d3aed
brieml <blopez-30@hotmail.com>
2024-12-13 18:04:38 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useRoutes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { ThemeSettings } from './theme/Theme';
+import RTL from './layouts/full/shared/customizer/RTL';
+import ScrollToTop from './components/shared/ScrollToTop';
+import Router from './routes/Router';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import LoadingBar from './LoadingBar';
+
+function App() {
+  const routing = useRoutes(Router);
+  const theme = ThemeSettings();
+  const customizer = useSelector((state) => state.customizer);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <LoadingBar />
+      <RTL direction={customizer.activeDir}>
+        <CssBaseline />
+        <ScrollToTop>{routing}</ScrollToTop>
+      </RTL>
+    </ThemeProvider>
+  );
+}
+
+export default App
